a565e0ba73ec10645758892b3aa9c4d69e1d5476
cyrine <syrine.benhassine@esprit.tn>
2022-03-28 18:29:31 +0200

    intial commit

---
+++ src/main/java/dariB/DariBApplication.java
@@ -0,0 +1,13 @@
+package dariB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DariBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DariBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dariB/DariBApplicationTests.java
@@ -0,0 +1,13 @@
+package dariB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DariBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
